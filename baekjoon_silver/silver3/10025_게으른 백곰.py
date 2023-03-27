58bff7a0c470cf6792ef101f1e2305e84d4a41d5
ash-silver <passlion@naver.com>
2023-03-27 18:03:39 +0900

    BOJ_10025_게으른 백곰.py

---
+++ baekjoon_silver/silver3/10025_게으른 백곰.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+pail = [0] * 1000001
+minIndex = 1000001
+maxIndex = 0
+answer = 0
+
+for _ in range(0, n):
+    g, x = map(int, input().split())
+    minIndex = min(x, minIndex)
+    maxIndex = max(x, maxIndex)
+    pail[x] = g
+step = 2 * k + 1
+slice = sum(pail[:step])
+answer = slice
+for i in range(step, maxIndex + 1):
+    slice += pail[i] - pail[i - step]
+    answer = max(slice, answer)
+print(answer)
