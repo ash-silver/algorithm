6e406b8f7d3bdcdf113794aaadecdcec4dd5bafd
ash-silver <passlion@naver.com>
2023-07-15 22:36:06 +0900

    BOJ_1002_터렛.py

---
+++ BOJ_silver/silver3/1002_터렛.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+answer = []
+for i in range(0, t):
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
+    distance = ((abs(x2 - x1))**2 + abs((y2 - y1))**2)**(1/2)
+
+    if distance == 0:
+        if r1 == r2:
+            answer.append(-1)
+        else:
+            answer.append(0)
+    elif distance < (r1 + r2):
+        answer.append(2)
+    elif distance == (r1 + r2):
+        answer.append(1)    
+    elif distance > (r1 + r2):
+        answer.append(0)
+    else:
+        answer.append(-1)
+
+for i in answer:
+    print(i)
